6815f7c5f13659a047f4beabb2a8988c57ac5655
aprianfirlanda <aprianfirlanda@gmail.com>
2024-09-30 11:32:44 +0700

    install tailwindcss typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import tailwindcssTypography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)'
+            }
+        }
     },
-  },
-  plugins: [
-    require('daisyui'),
-  ],
+    plugins: [tailwindcssTypography, daisyui]
 };
 export default config;
