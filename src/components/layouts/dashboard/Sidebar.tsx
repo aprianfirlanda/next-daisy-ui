b657d92823ab19008ebacab83bfd37d373b5e27b
aprianfirlanda <aprianfirlanda@gmail.com>
2024-10-01 14:13:49 +0700

    layout sidebar -> adjust menu height

--- src/components/layouts/dashboard/Sidebar.tsx
+++ src/components/layouts/dashboard/Sidebar.tsx
@@ -10,7 +10,7 @@ function Sidebar() {
             <div className="mb-5 flex justify-center">
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
-            <ul className="menu h-4/5 w-56 overflow-y-auto rounded-box bg-base-200">
+            <ul className="menu h-5/6 w-56 overflow-y-auto rounded-box bg-base-200">
                 {menus.map(({ icon, name, url }) => (
                     <MenuItem key={name} Icon={iconMapping[icon as IconName]} name={name} url={url} />
                 ))}
