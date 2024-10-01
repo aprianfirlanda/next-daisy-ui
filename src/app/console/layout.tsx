af295fae775606998a228f8c17c96e5b11a50c02
aprianfirlanda <aprianfirlanda@gmail.com>
2024-10-01 11:33:43 +0700

    update layout dashboard

--- src/app/console/layout.tsx
+++ src/app/console/layout.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import DashboardLayout from '@/components/layouts/dashboard/DashboardLayout';
+import DashboardNavbar from '@/components/layouts/dashboard/DashboardNavbar';
 
 function Layout({
     children
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <DashboardLayout>{children}</DashboardLayout>;
+    return (
+        <div className="m-0 flex h-dvh flex-col">
+            <DashboardNavbar />
+            <div className="flex flex-1 items-stretch">
+                <div className="w-52 overflow-y-auto p-4">sidebar</div>
+                <div className="flex w-full flex-col overflow-y-auto">
+                    <div className="p-4">header</div>
+                    <div className="p-4">{children}</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Layout;
